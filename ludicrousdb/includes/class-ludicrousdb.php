6314b35b060e69c0d0fb28838771c7656d3098a7
John James Jacoby <johnjamesjacoby@me.com>
2024-06-18 15:30:09 -0500

    PHPCS: Remove unused $new_link

--- ludicrousdb/includes/class-ludicrousdb.php
+++ ludicrousdb/includes/class-ludicrousdb.php
@@ -961,12 +961,10 @@ class LudicrousDB extends wpdb {
 	protected function single_db_connect( $dbhname, $host, $user, $password ) {
 		$this->is_mysql = true;
 
-		/*
-		 * Deprecated in 3.9+ when using MySQLi. No equivalent
-		 * $new_link parameter exists for mysqli_* functions.
-		 */
-		$new_link     = defined( 'MYSQL_NEW_LINK' ) ? MYSQL_NEW_LINK : true;
-		$client_flags = defined( 'MYSQL_CLIENT_FLAGS' ) ? MYSQL_CLIENT_FLAGS : 0;
+		// Check client flags
+		$client_flags = defined( 'MYSQL_CLIENT_FLAGS' )
+			? MYSQL_CLIENT_FLAGS
+			: 0;
 
 		$this->dbhs[ $dbhname ] = mysqli_init();
 
