10a3de6aa8e5b0f0417de8e8802f04f197901434
John James Jacoby <johnjamesjacoby@me.com>
2024-06-18 15:05:46 -0500

    PHPCS: Prefer "__DIR__"

--- ludicrousdb.php
+++ ludicrousdb.php
@@ -36,8 +36,8 @@ if ( isset( $GLOBALS['wpdb'] ) ) {
 }
 
 // Required files
-require_once dirname( __FILE__ ) . '/ludicrousdb/includes/functions.php';
-require_once dirname( __FILE__ ) . '/ludicrousdb/includes/class-ludicrousdb.php';
+require_once __DIR__ . '/ludicrousdb/includes/functions.php';
+require_once __DIR__ . '/ludicrousdb/includes/class-ludicrousdb.php';
 
 // Set default constants
 ldb_default_constants();
