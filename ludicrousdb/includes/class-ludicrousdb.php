1b37a64035c7014ada875a962acfab0523232e45
Joe Tipler <joe@pushentertainment.com>
2026-04-08 13:27:53 +0100

    Patch to use PHP8 SensitiveParameter on single_db_connect

--- ludicrousdb/includes/class-ludicrousdb.php
+++ ludicrousdb/includes/class-ludicrousdb.php
@@ -1321,7 +1321,14 @@ class LudicrousDB extends wpdb {
 	 *
 	 * @return bool|mysqli|resource
 	 */
-	protected function single_db_connect( $dbhname, $host, $user, $password ) {
+
+        protected function single_db_connect(
+               $dbhname,
+               $host,
+               $user,
+               #[\SensitiveParameter]
+               $password
+       ) {
 		$tcp_cache_key  = $host;
 		$this->is_mysql = true;
 
